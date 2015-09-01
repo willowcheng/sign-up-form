60037de409fa724a0c8e9687e81377db9d395803
willowcheng <e.wingus@Gmail.com>
2015-09-01 12:23:21 -0400

    Add password event for validation

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@
 //Solution: Hide and show them at appropriate times
 
 //Hide hints
+$("form span").hide();
+
+function passwordEvent(){
+    //Find out if password is valid
+    if($(this).val().length > 8) {
+        //Hide hint if valid
+        $(this).next().hide();
+    } else {
+        //else show hint
+        $(this).next().show();
+    }
+}
 
 //When event happens on password input
-    //Find out if password is valid
-        //Hide hint if valid
-        //else show hint
+$("#password").focus(passwordEvent).keyup(passwordEvent);
+
 
 //When event happens on confirmation input
     //Find out if password and confirmation match
